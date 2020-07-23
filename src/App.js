23d6d16a9b9b8aae0f9bbf6554972014f67bdd5d
RonaldVilorio <ronvilorio@gmail.com>
2020-07-23 13:35:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route component={Error} />
+        </Switch>
+      </header>
+    </div>
+  );
+}
+
+export default App;
