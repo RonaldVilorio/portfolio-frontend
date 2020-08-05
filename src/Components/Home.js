2d76b5a6b85be810e1dd21c30b1f352947ff86c7
RonaldVilorio <ronvilorio@gmail.com>
2020-08-05 15:32:21 -0400

    added own pic to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./Home.module.css";
+import image from "../assets/ron2.jpg";
 
 const Home = () => {
   return (
     <>
       <div className={styles.homeContainer}>
-        <div>Image</div>
-        <div>Ronald Vilorio \n Web Developer</div>
+        <div className={styles.imageDiv}>
+          <img src={image} alt="profile pic" width={250} />{" "}
+        </div>
+        <div>
+          <p>
+            Hey welcome to my personal site my name is Ronald Vilorio and I'm a
+            Full Stack Web Developer
+          </p>
+        </div>
       </div>
     </>
   );
