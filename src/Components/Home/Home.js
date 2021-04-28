08b0458f7f01ebd16fd34452fc0e746020c32ce3
RonaldVilorio <ronvilorio@gmail.com>
2021-04-28 15:17:58 -0400

    changed left to top etc, google fonts added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,12 +9,12 @@ class Home extends Component {
   render() {
     return (
       <div className={styles.homeContainer}>
-        <div className={styles.leftSide}>
+        <div className={styles.topSide}>
           <div className={styles.profileImage}>
             <img src={image} alt="profile pic" />{" "}
           </div>
         </div>
-        <div className={styles.rightSide}>
+        <div className={styles.bottomSide}>
           <p>
             Hey welcome to my personal site my name is Ronald Vilorio and I'm a
             Full Stack Web Developer
