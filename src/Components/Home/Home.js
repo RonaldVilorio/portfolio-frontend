9c1b29fe9cf94625d2068b002be35edb6ca645f7
RonaldVilorio <ronvilorio@gmail.com>
2021-04-30 19:17:06 -0400

    sfc for Home comp, took off borders in Proj comp

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,28 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Home.module.css";
 import image from "../../assets/ron2.jpg";
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className={styles.homeContainer}>
-        <div className={styles.topSide}>
-          <div className={styles.profileImage}>
-            <img src={image} alt="profile pic" />{" "}
-          </div>
-        </div>
-        <div className={styles.bottomSide}>
-          <p>
-            Hey welcome to my personal site my name is Ronald Vilorio and I'm a
-            Full Stack Web Developer
-          </p>
-        </div>
+
+const Home = () => {
+  return ( <div className={styles.homeContainer}>
+    <div className={styles.topSide}>
+      <div className={styles.profileImage}>
+        <img src={image} alt="profile pic" />{" "}
       </div>
-    );
-  }
+    </div>
+    <div className={styles.bottomSide}>
+      <p>
+        Hey welcome to my personal site my name is Ronald Vilorio and I'm a
+        Full Stack Web Developer
+      </p>
+      
+    </div>
+  </div> );
 }
+ 
 
 export default Home;
